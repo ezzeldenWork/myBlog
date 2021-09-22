5f9f8fdfbc33ba5d074bb340c63c76c6d1d4b46a
ezzeldenWork <ezzelden.work@gmail.com>
2021-09-22 17:00:02 +0200

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function () {
+  const quotApi = "./assets/quots/quots.json",
+    runApi = () => {
+      $.getJSON(quotApi, function (data) {
+        const randQuot = data[Math.floor(Math.random() * data.length)];
+        $(".qout_box .quot_prag").text(randQuot.quot);
+        $(".qout_box .quot_auther").text(randQuot.name);
+      });
+    };
 
-  
+  runApi();
 
-
+  $(".quot_relode").click(function () {
+    runApi();
+  });
 });
