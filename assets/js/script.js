494933abeb24b73992d2224698fcc99188bdccc6
ezzeldem <ezzelden06@gmail.com>
2021-09-24 10:48:43 +0200

    new svg img

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,13 +15,20 @@ $(document).ready(function () {
   });
 
 
-  $(".color_box ul li span").click(function(){
-    const theme_color = $(this).data("color");
-    $("html").attr('data-theme',theme_color)
+  // opne Setting Box
+  $(".color_box .opne_icon").click(function () {
+    $(".color_box").toggleClass("color_box--opne");
   })
 
-  $(".color_box .opne_icon").click(function(){
-    $(".color_box").toggleClass("color_box--opne")
+  // Chenge Theme color 
+  $(".color_box ul li span").click(function () {
+    const theme_color = $(this).data("color");
+    $("html").attr('data-theme', theme_color);
+    $(".sulod_sun").addClass("sulod_sun--animation");
+    setTimeout(function () { 
+      $(".sulod_sun").removeClass("sulod_sun--animation");
+    }, 1500);
   })
+
 
 });
