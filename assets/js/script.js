8af0cd90157d17de3d1a84936d03df8bd93956f7
ezzeldem <ezzelden06@gmail.com>
2021-09-25 10:07:16 +0200

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,18 +19,32 @@ $(document).ready(function () {
 
 
   // opne Setting Box
-  $(".color_box .opne_icon").click(function () {
+  $(".color_box .opne_icon").click(function (e) {
+    e.stopPropagation();
     $(".color_box").toggleClass("color_box--opne");
+  })
+  $(".color_box").click(function (e) {
+    e.stopPropagation();
   })
 
   // Chenge Theme color 
-  $(".color_box ul li span").click(function () {
+  $(".color_box .color-list li span").click(function () {
     const theme_color = $(this).data("color");
     $("html").attr('data-theme', theme_color);
     $(".sulod_sun").addClass("sulod_sun--animation");
     setTimeout(function () {
       $(".sulod_sun").removeClass("sulod_sun--animation");
     }, 1500);
+  });
+  $(".color_box .font_list li").click(function () {
+    const theme_color = $(this).attr("class");
+    $("html").attr('data-font', theme_color);
+  });
+
+  // body Click
+
+  $("body").click(function () {
+    $(".color_box").removeClass("color_box--opne");
   })
 
 
