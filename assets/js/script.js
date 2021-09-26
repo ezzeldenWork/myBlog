f8635c464c7d7930af7f0d11b9f328e86c3cd302
ezzeldenWork <ezzelden.work@gmail.com>
2021-09-26 15:23:19 +0200

    new style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 const quotApi = "./assets/quots/quots.json",
   runApi = () => {
     $.getJSON(quotApi, function (data) {
@@ -9,51 +8,50 @@ const quotApi = "./assets/quots/quots.json",
   };
 
 $(document).ready(function () {
-
-
   runApi();
+  $("html").attr("data-theme", localStorage.getItem("pageColor"));
 
   // body Click
 
   $("body").click(function () {
     $(".color_box").removeClass("color_box--opne");
-  })
-
+  });
 
   // random the quot
   $(".quot_relode").click(function () {
     runApi();
   });
 
-
   // opne Setting Box
   $(".color_box .opne_icon").click(function (e) {
     e.stopPropagation();
     $(".color_box").toggleClass("color_box--opne");
-  })
+  });
   $(".color_box").click(function (e) {
     e.stopPropagation();
-  })
+  });
 
-  // Chenge Theme color 
+  // Chenge Theme color
   $(".color_box .color-list li span").click(function () {
     const theme_color = $(this).data("color");
-    $("html").attr('data-theme', theme_color);
+    $("html").attr("data-theme", theme_color);
     $(".sulod_sun").addClass("sulod_sun--animation");
     setTimeout(function () {
       $(".sulod_sun").removeClass("sulod_sun--animation");
     }, 1500);
+
+    // for localStorage
+    localStorage.setItem("pageColor", theme_color);
   });
+
   $(".color_box .font_list li").click(function () {
     const theme_color = $(this).attr("class");
-    $("html").attr('data-font', theme_color);
+    $("html").attr("data-font", theme_color);
   });
-
 
   // opne Menu
-  $(".ez_header .opne_icon i").click(function (e) { 
+  $(".ez_header .opne_icon i").click(function (e) {
     e.preventDefault();
-    $(".ez_header .nav_list").slideToggle()
+    $(".ez_header .nav_list").slideToggle();
   });
-  
 });
